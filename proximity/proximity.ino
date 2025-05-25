d9e5d86a75f2ea1c1a73e026606599ef9f821bfa
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-05-25 08:52:54 -0400

    implement more communication stuff

--- proximity/proximity.ino
+++ proximity/proximity.ino
@@ -1,11 +1,21 @@
 #include <Wire.h>
 
+enum I2cDataMode
+{
+  I2C_MODE_ALARM_STATUS,
+  I2C_MODE_CHECK_PASSCODE,
+  I2C_MODE_CHECK_RFID,
+  I2C_MODE_INVALID_MODE,
+};
+
 const int trigPin = 9;
 const int echoPin = 10;
-
 const int buzzerPin = 12;
 
 const int i2cAddress = 55;
+const String passcode = "1234";
+
+bool alarmTripped = false;
 
 void setup()
 {
@@ -15,6 +25,72 @@ void setup()
 
   Serial.begin(9600);
   Wire.begin(i2cAddress);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+}
+
+I2cDataMode currentI2cMode = I2C_MODE_ALARM_STATUS;
+bool lastI2cRequestStatus = false;
+
+I2cDataMode getI2cMode(int firstByte)
+{
+  switch (firstByte)
+  {
+  case 0:
+    return I2C_MODE_ALARM_STATUS;
+
+  case 1:
+    return I2C_MODE_CHECK_PASSCODE;
+
+  case 2:
+    return I2C_MODE_CHECK_RFID;
+
+  default:
+    return I2C_MODE_INVALID_MODE;
+  }
+}
+
+// Recieve data to verify and prepare data to return
+void receiveEvent(int bytes)
+{
+  if (bytes == 0)
+  {
+    return;
+  }
+
+  int modeByte = Wire.read();
+  bytes -= 1;
+
+  currentI2cMode = getI2cMode(modeByte);
+
+  switch (currentI2cMode)
+  {
+  case I2C_MODE_ALARM_STATUS:
+    break;
+
+  default:
+    break;
+  }
+}
+
+void requestEvent()
+{
+  switch (currentI2cMode)
+  {
+  case I2C_MODE_ALARM_STATUS:
+    if (alarmTripped)
+    {
+      Wire.write(0);
+    }
+    else
+    {
+      Wire.write(1);
+    }
+    break;
+
+  default:
+    break;
+  }
 }
 
 float getSensorDistance()
@@ -51,8 +127,6 @@ void buzzerOff()
 {
   digitalWrite(buzzerPin, LOW);
 }
-
-bool alarmTripped = false;
 
 void loop()
 {
